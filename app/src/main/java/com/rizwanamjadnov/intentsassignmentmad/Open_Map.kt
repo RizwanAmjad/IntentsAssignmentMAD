5ecf19112cf255c0c41f2d92de42cf06f8ee5be6
kashir <hasnainkashir6105@gmail.com>
2021-12-13 08:49:38 +0500

    Add 2nd Intent Activity (OPEN MAP) >>Kashir Hasnain

---
+++ app/src/main/java/com/rizwanamjadnov/intentsassignmentmad/Open_Map.kt
@@ -0,0 +1,11 @@
+package com.rizwanamjadnov.intentsassignmentmad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Open_Map : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_map)
+    }
+}
